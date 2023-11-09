58626330afdfcbb964504911c0ddc99de763f798
soyeon <bmesjl5700@gmail.com>
2023-11-09 15:54:53 -0800

    Refactor Code

---
+++ Pill Reminder/Views/GreetingHeadLineView.swift
@@ -0,0 +1,18 @@
+//
+//  GreetingHeadLineView.swift
+//  Pill Reminder
+//
+//  Created by Soyeon Lee on 11/9/23.
+//
+
+import SwiftUI
+
+struct GreetingHeadLineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GreetingHeadLineView()
+}
---
+++ Pill Reminder/Views/WeekView.swift
@@ -0,0 +1,18 @@
+//
+//  WeekView.swift
+//  Pill Reminder
+//
+//  Created by Soyeon Lee on 11/9/23.
+//
+
+import SwiftUI
+
+struct WeekView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeekView()
+}
